296abaed04e179ff1793caa93498ed5338aa1475
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-04-24 15:29:40 +0200

    Add functions for calculating the neares neighbors of a given vector.
    Because of the translational symmetry of the lattice, and our end goal
    of calculating the bloch matrices, the functions implemented are only
    valid for vectors that reside within the first unit cell (i.e. the unit
    cell found the origin).

--- src/CrystalLattice.jl
+++ src/CrystalLattice.jl
@@ -1,11 +1,11 @@
 module CrystalLattice
 using FromFile
-# include(joinpath("..", "utils", "CrystalIO.jl"))
+using LinearAlgebra: norm
 @from "BravaisLattice.jl" using BravaisLattice: Bravais
 @from "BravaisLattice.jl" import BravaisLattice.getName
 @from "BravaisLattice.jl" import BravaisLattice.getVecs
 @from "../utils/CrystalIO.jl" using CrystalIO: parseCrystalConf as getCrystal
-export Crystal, getName, getSize, getLattice, getVecs
+export Crystal, getName, getSize, getLattice, getVecs, getPrimitiveNearestNeighbors
 
 struct Crystal
   name::String
@@ -37,6 +37,63 @@ function getVecs(crystal::Crystal)
   bravaisvecs = getVecs(lattice)
   basisvecs = crystal.basis
   return basisvecs, bravaisvecs
+end
+
+# This function has a number of parameters simply because it only serves
+# as a helper function to getPrimitiveNearestNeighbors, and the paramters shouldn't
+# be recalculated if they don't change across multiple calls.
+function getNearestNeighbors(crystal::Crystal, latticeiter, currentvec, dim, basis)
+  #=
+  The nearest neighbor list here is a boolean list containing true at position
+  n[j*basis + i] if basisvecs[:, i] + bravaisvecs[:, j] is a nearest neighbor 
+  of the currentvector.
+  =#
+  nnarray = Vector{Bool}(undef, (2*dim + 1)*basis)
+  minidx = 0
+  min::Float64 = NaN64
+  for (idx, (primitive, basisvec)) in enumerate(latticeiter)
+      position = primitive + basisvec
+      d = norm(position - currentvec)
+      
+      if isapprox(d, 0) # case: position is the currentvec
+          nnarray[idx] = false
+      elseif (isnan(min) || (!isapprox(min - d, 0) && d < min)) # case: position is true new nn
+          min = d
+          minidx = idx
+          nnarray[idx] = true
+      elseif isapprox(min - d, 0) # case: position is equally distant from currentvec as last nn
+          nnarray[idx] = true
+      else # case: position is further from currentvec as last nn
+          nnarray[idx] = false
+      end
+  end
+
+  # minidx stores the index of the first true nearest neighbor since findNearestNeighbor 
+  # updates minidx every time a point is found whos distance to the point stored at bidx
+  # is strictly smaller then the last smallest distance found. For this reason all true
+  # assignments to the nnarray list coming after minidx are correct and reflect true 
+  # nearest neighbors while all true assignments coming before this index are incorrect
+  # and must be changed to false.
+  nnview = @view nnarray[1:minidx - 1]
+  fill!(nnview, false)
+  return nnarray
+end
+
+function getPrimitiveNearestNeighbors(crystal::Crystal)::Matrix{Bool}
+
+  basisvecs, bravaisvecs = getVecs(crystal)
+  dim = size(basisvecs, 1)
+  basis = size(basisvecs, 2)
+  bravaisvecs = cat(zeros(dim), bravaisvecs, -bravaisvecs, dims = 2)
+  latticeiter = Iterators.product(eachcol(basisvecs), eachcol(bravaisvecs))
+  nnarrays = []
+  for basisvec in eachcol(basisvecs)
+    nnarray = getNearestNeighbors(crystal, latticeiter, basisvec, dim, basis)
+    push!(nnarrays, nnarray)
+  end
+  nnarrays = cat(nnarrays..., dims = 2)
+
+  return nnarrays
 end
 
 end
