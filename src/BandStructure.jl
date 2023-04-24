c6188d4ae7cb09b086ef268bb2e6ff9b201b4f26
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-04-24 15:34:43 +0200

    Add getHoppingMatrix in BandStructure.jl.
    The getHoppingMatrix function is added to the BandStructure.jl module.
    This function calculates the hopping matrix from the unit cell at the
    origin to one of the neighboring unit cells labeled by an integer.
    Valid labels for a unit cell are in the range 0:2 * dim. The label 0
    designates the unit cell at the origin, the labels 1:dim designate the
    unit cells given by the primitive vectors, bravaisvec[:,1:dim] and the
    labels in the range dim + 1:2 * dim desiginate the antiparallel
    primitive vectors -bravaisvec[:, 1:dim].
    
    Again only hopping matrices from the unit cell at the origin to another
    neighboring unit cell are considered because these are the only matrices
    that emerge in the calculation of the bloch matrices in a tight binding
    model.

--- src/BandStructure.jl
+++ src/BandStructure.jl
@@ -1,16 +1,18 @@
 module BandStructure
-println("test")
-#=
-using ConfParser
-using Base.Filesystem: joinpath
-
-include("BravaisLattice.jl")
-using .BravaisLattice: BravaisLattice
-global CONFIGPATH = "configs.ini"
-conf = ConfParse(CONFIGPATH)
-parse_conf!(conf)
-confroot = retrieve(conf, "Configurations", "root")
-confpath = joinpath(confroot, "bravais.ini")
-b = BravaisLattice("Triangle", confpath)
-=#
+using FromFile
+@from "CrystalLattice.jl" using CrystalLattice
+export getHoppingMatrix
+# This function assumes a tight binding model with only nearest neighbor hopping.
+# Furthermore we assume that the nearest neighbors are of an equal displacement 
+# from the point as every other point.
+function getHoppingMatrix(crystal::Crystal, cell::Int, nnarrays::Matrix{Bool})
+  basisvecs, _ = getVecs(crystal)
+  basis = size(basisvecs, 2)
+  hopping = zeros(basis, basis)
+  ij = Iterators.product(1:basis, 1:basis)
+  for (i, j) in ij
+    hopping[i, j] = nnarrays[cell*basis + i, j] ? -1 : 0
+  end
+  return hopping
+end
 end
