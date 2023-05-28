00f80b6099311322c93589cca4837dea0f4b1b59
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-05-28 12:27:17 +0200

    A basic electron lattice module was added.
    This module serves the purpose of iteratively adding structure to the
    underlying bravais lattice. The bravais lattice is the most basic
    structure consisting of simply the bravais lattice vectors, the crystal
    structure consists of a bravais lattice with a basis, and the electron
    lattice is a population of electrons with a given number of states at
    each site.

---
+++ src/ElectronLattice.jl
@@ -0,0 +1,37 @@
+module ElectronLattice
+using FromFile
+@from "CrystalLattice.jl" using CrystalLattice
+@from "CrystalLattice.jl" import CrystalLattice.getName
+export ElectronCrystal, getName, getCrystal, getStatesPerSite, getElectronNumber
+
+# The true states per site number is 2 * statespersite corresponding
+# to the spin degree of freedom.
+struct ElectronCrystal
+    name::String
+    crystal::Crystal
+    statespersite::Int32
+    population::Int64
+end
+
+function ElectronCrystal(name::String, crystal::String, statespersite::Int, population::Int, crystalfile::String, bravaisfile::String)
+    crystal = Crystal(crystal, crystalfile, bravaisfile)
+    ElectronCrystal(name, crystal, statespersite, population)
+end
+
+function getName(ecrystal::ElectronCrystal)
+    return ecrystal.name
+end
+
+function getCrystal(ecrystal::ElectronCrystal)
+    return ecrystal.crystal
+end
+
+function getStatesPerSite(ecrystal::ElectronCrystal)
+    return ecrystal.statespersite
+end
+
+function getElectronNumber(ecrystal::ElectronCrystal)
+    return ecrystal.population
+end
+
+end
