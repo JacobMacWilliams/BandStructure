419eb420ac8ee3a33b52ad41cb780e596f7874dc
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-06-12 12:20:19 +0200

    Add the Embeddings.jl utility.
    This module contains functions to transform to and from operators
    operating on the fock space with single particle states labeled by
    an arbitrary set of quantum numbers (i, j, k, l....) to the operators
    operating on the fock space with single particle states labeled by a
    single integer (n).

---
+++ utils/Embeddings.jl
@@ -0,0 +1,67 @@
+module Embeddings
+export embedinlist,
+       embedinmatrix,
+       extractfromlist,
+       extractfromatrix
+       
+# Assuming the first index of state changes the fastest along the rows
+# the second the second fastest along the rows...etc.
+function embedinlist(state::Tuple, sizes::Tuple)
+    idx = 1
+    size = 1
+    for (i, qnum) in enumerate(state)
+        idx += (qnum - 1) * size 
+        size *= sizes[i]
+    end
+    return idx
+end
+
+function embedinmatrix(tostate::Tuple, fromstate::Tuple, sizes::Tuple)
+    row = embedinlist(tostate, sizes)
+    col = embedinlist(fromstate, sizes)
+    return (row, col)
+end
+
+function extractfromlist(state::Int, sizes::Tuple)
+    totalstates = prod(sizes)
+    if state > totalstates
+        error("The state number provided is incompatible with the total" +
+              "degrees of freedom of the system.")
+    end
+    
+    state = state - 1
+    smallstep = 1
+    counted = 0
+    
+    extstate = zeros(length(sizes))
+    for (i, n) in enumerate(sizes)
+        
+        largestep = n * smallstep
+        stepsleft = mod(state, largestep) - counted
+        if stepsleft == 0
+            smallstep = largestep
+            continue
+        end
+
+        steps = stepsleft / smallstep
+        extstate[i] = steps
+        counted += stepsleft
+
+        if counted == state
+            break
+        end
+
+        smallstep = largestep
+    end
+
+    extstate = extstate .+ 1
+    return Tuple(extstate)
+end
+
+function extractfromatrix(tostate::Int, fromstate::Int, sizes::Tuple)
+    extractedtostate = extractfromlist(tostate, sizes)
+    extractedfromstate = extractfromlist(fromstate, sizes)
+    return (extractedtostate, extractedfromstate)
+end
+
+end
