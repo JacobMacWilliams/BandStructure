e862ddf1278f235fd215237731dca88af6f9c4a8
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-06-13 15:21:21 +0200

    Add distributions function in Distributions.jl
    In this module the fermi and bose distributions functions are contained.

---
+++ src/Distributions.jl
@@ -0,0 +1,19 @@
+module Distributions
+export fermidistribution,
+       bosedistribution
+
+function fermidistribution(e::Float64, beta::Float64, mu::Float64)
+    erel = e - mu
+    ex = exp(-beta * erel)
+    n = 1 / (ex + 1)
+    return n
+end
+
+function bosedistribution(e::Float64, beta::Float64, mu::Float64)
+    erel = e - mu
+    ex = exp(-beta * erel)
+    n = 1 / (ex - 1)
+    return n
+end
+
+end
