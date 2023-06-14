2bb31b771a2140be73bc79d0a5fef5ac4719d1bd
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-06-14 12:31:03 +0200

    Fix small bugs in Rotations.jl.
    export the function rotate2d, add types hints to rotate2d signature,
    correct typo in variable name.

--- utils/Rotations.jl
+++ utils/Rotations.jl
@@ -1,8 +1,8 @@
 module Rotations
-using Rotations
+export rotate2d
 
-function rotate2d(v::Vector, rad)
-    if length(v2d) != 2
+function rotate2d(v::Vector{Float64}, rad::Float64)
+    if length(v) != 2
         error("This function is only intended for rotations of 2d vectors.")
     end
     rot = [cos(rad) -sin(rad); sin(rad) cos(rad)]
