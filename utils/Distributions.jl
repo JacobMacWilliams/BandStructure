caabf7d4aec345fa0a2ab9bcc349617d420de131
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-06-15 11:05:06 +0200

    Reorder arguments in distribution function of Distributions.jl
    instead of e, beta, mu the arguments are now ordered e, mu, beta. It is
    more intuitive that the energy arguments belong together.

--- utils/Distributions.jl
+++ utils/Distributions.jl
@@ -2,14 +2,14 @@ module Distributions
 export fermidistribution,
        bosedistribution
 
-function fermidistribution(e::Float64, beta::Float64, mu::Float64)
+function fermidistribution(e::Float64, mu::Float64, beta::Float64, )
     erel = e - mu
     ex = exp(-beta * erel)
     n = 1 / (ex + 1)
     return n
 end
 
-function bosedistribution(e::Float64, beta::Float64, mu::Float64)
+function bosedistribution(e::Float64, mu::Float64, beta::Float64)
     erel = e - mu
     ex = exp(-beta * erel)
     n = 1 / (ex - 1)
