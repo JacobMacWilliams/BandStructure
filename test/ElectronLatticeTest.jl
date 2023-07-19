232fa0919b81cf7a2deb68d780b3c1f5adfa0180
Jacob MacWilliams <jacobmacwilliams@gmail.com>
2023-07-19 16:32:32 -0400

    Remove constructHoppingMatrixTest from tests.
    This test was intended to test an older version of getHoppingMatrixTest
    and not the newer one. Furthermore, there doesn't seem to be to much
    sense in writing a newer version now because we know the function works
    and test are supposed to test behaviour and not simply whether or not
    the function works correctly or not.

--- test/ElectronLatticeTest.jl
+++ test/ElectronLatticeTest.jl
@@ -20,23 +20,8 @@ function electronInit(efile::String, cfile::String, bfile::String)
     return c1 && c2 && c3
 end
 
-function constructHoppingMatrixTest(efile::String, cfile::String, bfile::String)
-    model = "hubbard model"
-    ecrystal = ElectronCrystal(model, efile, cfile, bfile)
-    nnhopping = [1 1 15 5; 1 1 5 15; 1 1 1 1; 1 1 1 1]
-    localhopping = [7 1 15 5; 1 7 5 15; 15 5 7 1; 5 15 1 7]
-    nnarrays = getPrimitiveNearestNeighbors(getCrystal(ecrystal))
-    nnhoppingtest = getHoppingMatrixStructure(ecrystal, 1, nnarrays)
-    localhoppingtest = getHoppingMatrixStructure(ecrystal, 0, nnarrays)
-
-    c1 = (nnhopping == nnhoppingtest)
-    c2 = (localhopping == localhoppingtest)
-    return c1 && c2
-end
-
 bravaisconf = joinpath("conf", "bravais.default.toml")
 crystalconf = joinpath("conf", "crystal.default.toml")
 electronconf = joinpath("conf", "elattice.default.toml")
 @test electronInit(electronconf, crystalconf, bravaisconf)
-@test constructHoppingMatrixTest(electronconf, crystalconf, bravaisconf)
 end
